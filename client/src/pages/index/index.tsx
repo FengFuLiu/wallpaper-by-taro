79506c8c88b524c7c0bb67fe4ebd21b4df50059a
刘佳辉 <503630157@qq.com>
2021-12-11 16:27:14 +0800

    chore: 添加组件库

--- client/src/pages/index/index.tsx
+++ client/src/pages/index/index.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import { View } from '@tarojs/components';
 import Login from '../../components/login/index.weapp';
+import { Button } from '@taroify/core';
 
 export default function Index() {
   const [count, setCount] = useState(0);
 
   return (
     <View>
-      <View>sddsssss</View>
+      <Button>sddsssss</Button>
       <Login />
     </View>
   );
